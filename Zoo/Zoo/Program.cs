2f68b3df08ebaa0bbdb599a50af9d841e54606bd
Elain Wong <elainwongleo@gmail.com>
2025-09-17 23:15:08 -0400

    Add Zoo folder

---
+++ Zoo/Zoo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+// RandomizeAnimals();
+// AssignGroup(); ---> var group = AssignGroup(); ---> string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+// var group = AssignGroup();
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
+
+string[,] AssignGroup(int groups=6)
+{
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+    int start = 0;
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
+    return result;
+}
+
+
+
